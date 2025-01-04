62097f0027fed7ba26b5e2700a45af4237c8d1f8
cas-pb <castillo.patriciab@gmail.com>
2025-01-04 11:10:56 +0800

    create function to remove existing  grid and replace with new grid size

--- script.js
+++ script.js
@@ -32,3 +32,21 @@ const makeGrid = (size) => {
 };
 
 makeGrid(16);
+
+function makeButton() {
+  button.addEventListener('click', (event) => {
+    let newGridSize = prompt('Enter a number between 1 to 100');
+    // converts string numbers to integer.e.g "123" (string) to 123
+    newGridSize = parseInt(newGridSize);
+
+    if (newGridSize && newGridSize > 0 && newGridSize <= 100) {
+      makeGrid(newGridSize);
+    }
+
+    else {
+      alert('Please enter a valid number between 0 and 100');
+    }
+  })
+}
+
+makeButton();
